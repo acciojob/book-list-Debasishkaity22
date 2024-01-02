a9289a644b4e0f060a5e0a9ac30217a75829b7fc
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2024-01-02 23:06:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ form.addEventListener("click", function (e) {
     let author = document.querySelector("#author").value;
     let isbn = document.querySelector("#isbn").value;
     if (title && author && isbn) {
-        let tr = document.createElement("tr");
-        tr.innerHTML=`<td>${title}</td>
+        let newRow = document.createElement("tr");
+        newRow.innerHTML=`
+		<td>${title}</td>
         <td>${author}</td>
         <td>${isbn}</td>
         <td class="delete" onclick="deleteRow(this)">X</td>
       `;
-        tableBody.appendChild(tr);
+        tableBody.appendChild(newRow);
         form.reset();
     }
 
