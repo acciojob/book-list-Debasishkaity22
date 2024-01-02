23f1af4331086998b988ad5e769d14fcfbddc4e5
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2024-01-02 22:55:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let form = document.querySelector("form");
+let tableBody = document.querySelector("#book-list")
+form.addEventListener("click", function (e) {
+    e.preventDefault();
+    let title = document.querySelector("#tittle").value;
+    let author = document.querySelector("#author").value;
+    let isbn = document.querySelector("#isbn").value;
+    if (title && author && isbn) {
+        let tr = document.createElement("tr");
+        tr.innerHTML=`<td>${title}</td>
+        <td>${author}</td>
+        <td>${isbn}</td>
+        <td class="delete" onclick="deleteRow(this)">X</td>
+      `;
+        tableBody.appendChild(tr);
+        form.reset();
+    }
+
+});
+function deleteRow(row){
+  let rowDel=row.parentNode.parentNode.rowIndex;
+  tableBody.deleteRow(rowDel);
+}
